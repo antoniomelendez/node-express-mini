873e46611ce2e58dc55129a001625d8d2ec5e8be
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-08-09 21:44:10 -0600

    Hangout Node completed.

--- src/app.js
+++ src/app.js
@@ -13,7 +13,41 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
-
+const words = readWords();
+const index = Math.floor(Math.random() * words.length);
+const finalWord = words[index];
+const guesses = {};
 // TODO: your code to handle requests
+server.post('/guess', (req, res) => {
+  const letter = req.body.letter;
+  if (!letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error:'Must provide a guessed letter'});
+    return;
+  }
+  if (letter.length !== 1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error:'Guess must be a single letter'});
+    return;
+  }
+  if (guesses[letter]) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: 'You have already guess this letter'});
+    return;
+  }
+  guesses[letter] = true;
+  res.json(guesses);
+})
+
+server.get('/', (req, res) => {
+  const wordSoFarArray = Array.from(finalWord).map((letter) => {
+    if(guesses[letter]) {
+      return letter;
+    }
+    return '-';
+  })
+  wordSoFar = wordSoFarArray.join('');
+  res.json({wordSoFar, guesses});
+});
 
 server.listen(3000);
